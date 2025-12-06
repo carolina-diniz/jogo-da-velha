f5db86dfa038173bdcc119142ed6837ea03888a2
Carolina Diniz <caroldinizc21@gmail.com>
2025-12-06 01:51:36 -0300

    feat(*): cria rotas iniciais

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RoutesConfig } from '~core';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function main() {
+  const routes = RoutesConfig.getAllRoutes();
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>{routes}</BrowserRouter>
+    </StrictMode>,
+  );
+}
+
+main();
---
+++ src/modules/pages/game/game.route.ts
@@ -0,0 +1,7 @@
+import type { RouteConfig } from '~core';
+import { GamePage } from './game.page';
+
+export default {
+  path: '/game',
+  element: GamePage(),
+} as RouteConfig;
