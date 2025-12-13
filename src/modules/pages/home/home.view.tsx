4dd774176741e99651f94fa83c058c5e742f8ac5
Carolina Diniz <caroldinizc21@gmail.com>
2025-12-13 16:34:31 -0300

    feat(pages/home): adiciona titulo

--- src/modules/pages/home/home.view.tsx
+++ src/modules/pages/home/home.view.tsx
@@ -1,10 +1,11 @@
 import type { JSX } from 'react';
-import { Avatar } from './components/avatar/avatar.component';
+import { Avatar } from './components';
 import './home.style.scss';
 
 export function HomeView(): JSX.Element {
   return (
     <div className="home__menu-content">
+      <h1 className="home__title">JOGO DA VELHA</h1>
       <Avatar />
       <div className="home__divider"></div>
     </div>
