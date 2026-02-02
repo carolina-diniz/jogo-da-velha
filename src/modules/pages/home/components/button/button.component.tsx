3f3a31bcefb865ab471b61a1c1ba3cef65bb2858
Wilbsom gs <willsuporte@gmail.com>
2026-02-02 00:22:40 -0300

    feat(pages/home): adiciona botões à home

--- src/modules/pages/home/components/button/button.component.tsx
+++ src/modules/pages/home/components/button/button.component.tsx
@@ -3,11 +3,13 @@ import './button.style.scss';
 
 export function Button({ name, img }: { name: string; img: string }): JSX.Element {
   return (
-    <div className="button__container">
-      <div className="button__img-container">
-        <img className="button__img" src={img} alt="plus icon" />
+    <div className="button__container-out">
+      <div className="button__container">
+        <div className="button__img-container">
+          <img className="button__img" src={img} alt="plus icon" />
+        </div>
+        <p className="button__label">{name}</p>
       </div>
-      <p className="button__label">{name}</p>
     </div>
   );
 }
